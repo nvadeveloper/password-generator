545a64ecfa235da7867f23f766182ff5afdae895
Nikita <101108890+nvadeveloper@users.noreply.github.com>
2024-02-01 23:22:59 +0300

    add icon lib and some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import { BsGithub } from 'react-icons/bs';
 import PasswordSection from '@/components/PasswordSection';
 import { Toaster } from 'react-hot-toast';
 
 export default function Home() {
     return (
-        <main className="max-w-96 mx-auto p-4">
-            <h1>Password generator</h1>
+        <main className="max-w-96 mx-auto p-4 mt-6">
+            <div className="flex justify-between mb-2">
+                <h1>Password generator</h1>
+                <a
+                    href="https://github.com/nvadeveloper/password-generator"
+                    className="text-[12px] text-gray-400 flex items-center">
+                    Source code
+                    <span>
+                        <BsGithub className="w-5 h-5 pl-1" />
+                    </span>
+                </a>
+            </div>
+
             <PasswordSection />
             <Toaster position="top-right" reverseOrder={false} />
         </main>
--- src/components/PasswordSection.tsx
+++ src/components/PasswordSection.tsx
@@ -1,12 +1,13 @@
 'use client';
-import CopyIcon from '@/components/CopyIcon';
-import RefreshIcon from '@/components/RefreshIcon';
+import { BsFiles } from 'react-icons/bs';
+import { BsArrowRepeat } from 'react-icons/bs';
+
 import { nanoid } from 'nanoid';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
 
 export default function PasswordSection() {
-    const [currentPassword, setCurrentPassword] = useState(nanoid(8));
+    const [currentPassword, setCurrentPassword] = useState(nanoid(12));
 
     return (
         <section>
@@ -16,23 +17,23 @@ export default function PasswordSection() {
                     onChange={(e) => setCurrentPassword(e.target.value)}
                     type="string"
                     name="password"
-                    className="border-2 border-black py-2 px-3 rounded-md text-xl w-full relative"
+                    className="border-2 border-black py-2 px-3 rounded-md text-xl w-full relative focus:outline-none focus:ring focus:ring-violet-200"
                 />
                 <div className="absolute right-2 top-2 bg-white">
                     <button
-                        className="p-0.5"
+                        className="p-1"
                         onClick={() => {
                             navigator.clipboard.writeText(currentPassword);
-                            toast('Сopied! 🙌');
+                            toast('Password copied! 🙌');
                         }}>
-                        <CopyIcon />
+                        <BsFiles className="w-5 h-5 hover:text-gray-500" />
                     </button>
                     <button
-                        className="p-0.5"
+                        className="p-1"
                         onClick={() => {
-                            setCurrentPassword(nanoid(8));
+                            setCurrentPassword(nanoid(12));
                         }}>
-                        <RefreshIcon />
+                        <BsArrowRepeat className="w-5 h-5 hover:text-gray-500" />
                     </button>
                 </div>
             </div>
