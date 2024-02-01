524df1d04373bbe6b6665b2112e7b1502d3d1019
Nikita <101108890+nvadeveloper@users.noreply.github.com>
2024-02-01 23:04:00 +0300

    add toast lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import PasswordSection from '@/components/PasswordSection';
+import { Toaster } from 'react-hot-toast';
 
 export default function Home() {
     return (
-        <main>
+        <main className="max-w-96 mx-auto p-4">
             <h1>Password generator</h1>
             <PasswordSection />
+            <Toaster position="top-right" reverseOrder={false} />
         </main>
     );
 }
--- src/components/PasswordSection.tsx
+++ src/components/PasswordSection.tsx
@@ -1,23 +1,40 @@
+'use client';
 import CopyIcon from '@/components/CopyIcon';
 import RefreshIcon from '@/components/RefreshIcon';
 import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
 
 export default function PasswordSection() {
+    const [currentPassword, setCurrentPassword] = useState(nanoid(8));
+
     return (
         <section>
-            <div>
+            <div className="flex relative">
                 <input
-                    value={nanoid(8)}
+                    defaultValue={currentPassword}
+                    onChange={(e) => setCurrentPassword(e.target.value)}
                     type="string"
                     name="password"
-                    className="border-2 border-black py-2 px-3 rounded-md text-xl w-[350px]"
+                    className="border-2 border-black py-2 px-3 rounded-md text-xl w-full relative"
                 />
-                <button>
-                    <CopyIcon />
-                </button>
-                <button>
-                    <RefreshIcon />
-                </button>
+                <div className="absolute right-2 top-2 bg-white">
+                    <button
+                        className="p-0.5"
+                        onClick={() => {
+                            navigator.clipboard.writeText(currentPassword);
+                            toast('Сopied! 🙌');
+                        }}>
+                        <CopyIcon />
+                    </button>
+                    <button
+                        className="p-0.5"
+                        onClick={() => {
+                            setCurrentPassword(nanoid(8));
+                        }}>
+                        <RefreshIcon />
+                    </button>
+                </div>
             </div>
         </section>
     );
